9da4a58a87a83bc3b1ad7ee35a4181534dc08175
Victor Andrade <victor64gaming@gmail.com>
2025-11-17 21:52:52 -0300

    🎉 Configuração inicial

---
+++ src/main/java/com/unilopers/roupas/RoupasApplication.java
@@ -0,0 +1,13 @@
+package com.unilopers.roupas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoupasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoupasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unilopers/roupas/RoupasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unilopers.roupas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoupasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
