31514948113dced69adc65be0765ff08f347fc84
igordp2301 <igordominguespereira10@gmail.com>
2025-11-18 21:48:40 -0300

    :sparkles: Criando Embeddable ID do OrderItem utilizando OrderID e ProductID

---
+++ src/main/java/com/unilopers/roupas/domain/OrderItem.java
@@ -0,0 +1,20 @@
+package com.unilopers.roupas.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem {
+
+}
