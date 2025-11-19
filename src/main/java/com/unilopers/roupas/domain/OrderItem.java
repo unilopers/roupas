794905ba9bff1880f4c2d523f9275db633803f82
igordp2301 <igordominguespereira10@gmail.com>
2025-11-18 21:53:35 -0300

    :sparkles: Adicionado entidade OrderItem utilizando o embeddedid e chaves estrangeiras

--- src/main/java/com/unilopers/roupas/domain/OrderItem.java
+++ src/main/java/com/unilopers/roupas/domain/OrderItem.java
@@ -2,6 +2,7 @@ package com.unilopers.roupas.domain;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,12 +10,34 @@ import lombok.Setter;
 import java.time.LocalDate;
 import java.util.UUID;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 @Entity
 @Table(name = "tb_order_item")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class OrderItem {
 
+    @EmbeddedId
+    private OrderItemId id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("orderId")    
+    @JoinColumn(name = "order_id", nullable = false, foreignKey = @ForeignKey(name = "fk_orderitem_order"))
+    private Orders order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("productId")  
+    @JoinColumn(name = "product_id", nullable = false, foreignKey = @ForeignKey(name = "fk_orderitem_product"))
+    private Product product;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+
+    @Column(name = "unit_price", nullable = false)
+    private Double unitPrice;
+
+    @Column(name = "subtotal", nullable = false)
+    private Double subtotal;
 }
