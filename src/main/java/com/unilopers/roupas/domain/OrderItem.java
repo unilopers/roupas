2e89ae75b3c700c25ed2f159fe999a7e3812520b
Felipe Caldeira Akryghti <felipecaldeira2804@gmail.com>
2025-11-24 20:29:59 -0300

    🔥 remove imports não utilizados da entidade OrderItem

--- src/main/java/com/unilopers/roupas/domain/OrderItem.java
+++ src/main/java/com/unilopers/roupas/domain/OrderItem.java
@@ -4,12 +4,9 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
